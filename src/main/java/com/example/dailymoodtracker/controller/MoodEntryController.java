9059a8cb1ff10414b2ac7b99f8ae16097ecc93d2
Mikita Uladykousky <nikita.uladykouski@gmail.com>
2026-03-03 21:34:15 +0300

    checkstyle warnings fixed

--- src/main/java/com/example/dailymoodtracker/controller/MoodEntryController.java
+++ src/main/java/com/example/dailymoodtracker/controller/MoodEntryController.java
@@ -2,22 +2,23 @@ package com.example.dailymoodtracker.controller;
 import com.example.dailymoodtracker.dto.MoodEntryDto;
 import com.example.dailymoodtracker.mapper.MoodEntryMapper;
 import com.example.dailymoodtracker.service.MoodEntryService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.time.LocalDate;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 @RestController
 public class MoodEntryController {
-
     private final MoodEntryService service;
     private final MoodEntryMapper mapper;
-
     public MoodEntryController(MoodEntryService service, MoodEntryMapper mapper) {
         this.service = service;
         this.mapper = mapper;
     }
-
     /* http://localhost:8080/*/
     @GetMapping("/")
     public Map<String, Object> showUserInfo() {
@@ -26,15 +27,13 @@ public class MoodEntryController {
         response.put("userStatus", "Java enjouer");
         response.put("lastEntryDate", "2024-05-20");
         response.put("motivation", "no motivation today");
-        response.put("available_endpoints", Map.of("filter_by_date", "/api/moods?date=2024-05-20", "get_by_id", "/api/moods/{id}"));
+        response.put("available_endpoints", Map.of("filter_by_date",
+            "/api/moods?date=2024-05-20", "get_by_id", "/api/moods/{id}"));
         return response;
     }
     @GetMapping("/api/moods")
     public List<MoodEntryDto> getByDate(@RequestParam LocalDate date) {
-        return service.getByDate(date)
-            .stream()
-            .map(mapper::toDto)
-            .toList();
+        return service.getByDate(date).stream().map(mapper::toDto).toList();
     }
     @GetMapping("/api/moods/{id}")
     public MoodEntryDto getById(@PathVariable Long id) {
